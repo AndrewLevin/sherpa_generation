ce9113e15450fb33dd963ba3df87d7c0d3e2a238
Andrew <anlevin@cern.ch>
2014-08-21 14:58:01 +0200

    no commit message

--- setup.sh
+++ setup.sh
@@ -3,13 +3,16 @@ tar -xvf SHERPA-MC-2.1.1.tar.gz
 wget http://crd.lbl.gov/~dhbailey/mpdist/qd-2.3.13.tar.gz
 tar -xvf qd-2.3.13.tar.gz
 cd qd-2.3.13/
-./configure --enable-shared
+./configure --enable-shared --prefix=/afs/cern.ch/work/a/anlevin/sherpa_generation/qd_installation/
 make
+make install
 cd ../
 wget http://www.hepforge.org/archive/blackhat/blackhat-0.9.9.tar.gz
 tar -xzf blackhat-0.9.9.tar.gz
 cd blackhat-0.9.9
-
+./configure --with-QDpath="/afs/cern.ch/work/a/anlevin/sherpa_generation/qd_installation/" --prefix=/afs/cern.ch/work/a/anlevin/sherpa_generation/blackhat_installation/
+make 
+make install
 cd SHERPA-MC-2.1.1 
- ./configure --with-sqlite3=install
+./configure --with-sqlite3=install --enable-blackhat="/afs/cern.ch/work/a/anlevin/sherpa_generation/blackhat_installation/"
 make install
